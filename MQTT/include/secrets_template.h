74d6c1415ca3c884f4b90b5c62ab59006944bcf6
Colin Yang <36985531+MChoc@users.noreply.github.com>
2022-05-30 17:11:30 +1000

    Update secrets_template.h

--- MQTT/include/secrets_template.h
+++ MQTT/include/secrets_template.h
@@ -1,26 +1,30 @@
-// Create your own secrets.h in this folder and fill in the blanks
+// Create your own secrets.h in this folder and fill in the blanks.
+// The certificates are equivalent to those from
+// /Workshop/2. Device Registration/Certificates.png
 #include <pgmspace.h>
 
-#define SECRET
+// THINGNAME should be in the format EduKit_TEAMNAME
+// where TEAMNAME is the name of your team
 #define THINGNAME ""
+#define SECRET
 
 const char WIFI_SSID[] = "";
 const char WIFI_PASSWORD[] = "";
 const char AWS_IOT_ENDPOINT[] = "";
 
-// Amazon Root CA 1
+// AmazonRootCA1
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
 -----END CERTIFICATE-----
 )EOF";
 
-// Device Certificate
+// device_cert
 static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
 -----END CERTIFICATE-----
 )KEY";
 
-// Device Private Key
+// private
 static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
 -----END RSA PRIVATE KEY-----
