2f570b76f3bd918a2868a447d781c508a6aad082
Colin Yang <colin.yang96@gmail.com>
2022-05-31 20:42:20 +1000

    Make secrets more clear

--- MQTT/include/secrets_template.h
+++ MQTT/include/secrets_template.h
@@ -1,4 +1,5 @@
-// Create your own secrets.h in this folder and fill in the blanks.
+// Create a secrets.h file in this folder either by
+// renaming this file or copying and fill in the blanks.
 // The certificates are equivalent to those from
 // /Workshop/2. Device Registration/Certificates.png
 #include <Arduino.h>
@@ -16,20 +17,28 @@ const char WIFI_SSID[] = "";
 const char WIFI_PASSWORD[] = "";
 const char AWS_IOT_ENDPOINT[] = "a2fk4jnz30nap8-ats.iot.ap-southeast-2.amazonaws.com";
 
-// AmazonRootCA1
+// Fill in the below by copying the contents
+// of the keys you downloaded in Workshop 2. 
+// It is important to KEEP the
+// -----BEGIN CERTIFICATE----- and -----END CERTIFICATE-----
+
+// Copy the contents of AmazonRootCA1.cer
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
+COPY_YOUR_KEY_HERE
 -----END CERTIFICATE-----
 )EOF";
 
-// device_cert
+// Copy the contents of device_cert.crt
 static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
+COPY_YOUR_KEY_HERE
 -----END CERTIFICATE-----
 )KEY";
 
-// private
+// Copy the contents of private.key
 static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
+COPY_YOUR_KEY_HERE
 -----END RSA PRIVATE KEY-----
 )KEY";
